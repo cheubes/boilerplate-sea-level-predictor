14d7be0ef8043d5f6f24f47e6d2aeb29cecef6e0
cheubes <christophe@heubes.com>
2021-02-15 17:13:14 +0100

    Pass test_plot_title.

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,10 +4,10 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv (r'./epa-sea-level.csv', delimiter=',')
 
     # Create scatter plot
-
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
     # Create first line of best fit
 
@@ -16,8 +16,8 @@ def draw_plot():
 
 
     # Add labels and title
+    plt.title('Rise in Sea Level');
 
-    
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
