dd107b02a79a4c3b25d57c1fdf5829d09a1d76ec
cheubes <christophe@heubes.com>
2021-02-15 17:30:23 +0100

    Pass test_plot_lines.

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -14,10 +14,14 @@ def draw_plot():
     bestFit = linregress(df['Year'], df['CSIRO Adjusted Sea Level']);
     xBestFit = np.arange(df['Year'].min(), 2050,1)
     yBestFit = xBestFit * bestFit.slope + bestFit.intercept
-    plt.plot(xBestFit, yBestFit, color="green");
+    plt.plot(xBestFit, yBestFit, color='green');
 
     # Create second line of best fit
-
+    dfSince2000 = df[df['Year'] >= 2000]
+    bestFitSince2000 = linregress(dfSince2000['Year'], dfSince2000['CSIRO Adjusted Sea Level']);
+    xBestFitSince2000 = np.arange(2000, 2050)
+    yBestFitSince2000 = xBestFitSince2000 * bestFitSince2000.slope + bestFitSince2000.intercept
+    plt.plot(xBestFitSince2000, yBestFitSince2000, color='red');
 
     # Add labels and title
     plt.title('Rise in Sea Level');
